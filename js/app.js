f05a905d9fcce2ef3b40267576c28c8a3ace4e4e
Joe Marzullo <jmarzu13@gmail.com>
2016-08-09 10:30:45 -0700

    Switched to Angular Materialize

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-var app = angular.module('Gurame_Gurame', ['ui.router', 'ui.bootstrap']);
+var app = angular.module('Gurame_Gurame', ['ui.router', 'ui.materialize', 'ngLettering']);
 console.log('gurame gurame');
 
 app.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {

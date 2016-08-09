b389d7910d1f91dcee68b9bd02e34cc33b6dd5fe
Joe Marzullo <jmarzu13@gmail.com>
2016-08-08 21:46:21 -0700

    Express packages installed

--- controllers/MainCtrl.js
+++ controllers/MainCtrl.js
@@ -1,3 +1,8 @@
 app.controller('MainCtrl', ['$scope', function($scope) {
   console.log('in main controller');
+
+  $scope.showMessage = function() {
+    console.log('button clicked');
+  }
+
 }]);
